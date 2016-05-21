eb49bbc425e43c76dfb9955645b43280afa83272
AntonTarasovich <hammerhate@mail.ru>
2016-05-21 22:49:32 +0300

    Add class for counting distance between two points.

--- sandbox/src/main/java/ua/projects/javatests/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/projects/javatests/sandbox/MyFirstProgram.java
@@ -3,7 +3,17 @@ package ua.projects.javatests.sandbox;
 public class MyFirstProgram {
 	
 public static void main(String[] args) {
-	System.out.println("Hello, world!");
+
+    Point alpha = new Point(2, 5);
+    Point beta = new Point(6, 2);
+
+	System.out.println("Расстояние между двумя точками alpha (координаты " + alpha.x + ", " + alpha.y + ") и beta (координаты " + beta.x + ", "
+            + beta.y + ") на двумерной плоскости равно " + distance(alpha, beta) + ".");
+    System.out.println("Расстояние между двумя точками alpha (координаты " + alpha.x + ", " + alpha.y + ") и beta (координаты " + beta.x + ", "
+            + beta.y + ") на двумерной плоскости равно " + alpha.distance(beta) + ".");
 	}
-	
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
 }
---
+++ sandbox/src/main/java/ua/projects/javatests/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ua.projects.javatests.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2));
+    }
+}
